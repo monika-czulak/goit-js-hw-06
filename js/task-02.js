fea6f99677b720b943455e5f41a38a7a9e5aa1c8
monika-czulak <czulak.monika@gmail.com>
2022-12-01 14:01:45 +0100

    task-02 completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments']
+
+const list = document.querySelector('#ingredients')
+
+for (let i = 0; i < ingredients.length; i++) {
+	const listItem = document.createElement('li')
+	listItem.textContent = ingredients[i]
+	listItem.classList.add('item')
+	list.append(listItem)
+}
