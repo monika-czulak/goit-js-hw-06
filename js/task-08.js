b1bf01be4203c5972bf0a35fc0e0316874caed17
monika-czulak <czulak.monika@gmail.com>
2022-12-02 11:25:58 +0100

    task-08 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+	event.preventDefault()
+	const {
+		elements: { email, password },
+	} = event.currentTarget
+
+	if (email.value === '' || password.value === '') {
+		return alert('Please fill in all the fields!')
+	}
+
+	const loginData = new Object()
+	loginData.email = email.value
+	loginData.password = password.value
+	console.log(loginData)
+	event.currentTarget.reset()
+}
