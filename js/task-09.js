14c8d77cfa8d3cb33c00f3c32493b481b3b95198
monika-czulak <czulak.monika@gmail.com>
2022-12-02 12:01:17 +0100

    task-09 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`
 }
+
+const colorBtn = document.querySelector('.change-color')
+const text = document.querySelector('.color')
+const body = document.querySelector('body')
+
+colorBtn.addEventListener('click', () => {
+	body.style.backgroundColor = getRandomHexColor()
+	text.innerText = getRandomHexColor()
+})
