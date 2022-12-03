52f7ff215f97c8ead143d36ee666849d98e8da72
monika-czulak <czulak.monika@gmail.com>
2022-12-03 15:31:40 +0100

    task-10 completed

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`
 }
+
+const userInput = document.querySelector('#controls input')
+const boxesCollection = document.querySelector('#boxes')
+const createBtn = document.querySelector('button[data-create]')
+const destroyBtn = document.querySelector('button[data-destroy]')
+const amount = document.querySelector('input')
+
+function createBoxes(amount) {
+	for (let i = 0; i < amount; i++) {
+		console.log(amount)
+		const newBox = document.createElement('div')
+		boxesCollection.append(newBox)
+		let size = 30 + i * 10
+		newBox.style.width = `${size}px`
+		newBox.style.height = `${size}px`
+		newBox.style.backgroundColor = getRandomHexColor()
+	}
+}
+
+function destroyBoxes() {
+	boxesCollection.innerHTML = ''
+}
+
+createBtn.addEventListener('click', e => createBoxes(amount.value))
+destroyBtn.addEventListener('click', destroyBoxes)
